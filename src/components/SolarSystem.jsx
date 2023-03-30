99aaab76732a1176837c36619e1dba6e7452913e
Clinton Brito <clintoncsb@gmail.com>
2023-03-30 16:26:40 -0300

    Corrigindo o 4o requisito v2

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -5,7 +5,7 @@ export default class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <Title headline="planets" />
+        <Title headline="Planetas" />
       </div>
     );
   }

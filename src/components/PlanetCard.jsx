51c79d24ae6c835529016f60d9427212793ff785
Clinton Brito <clintoncsb@gmail.com>
2023-03-31 19:34:22 -0300

    CSS: PlanetCard & PlanetSystem | modifying positioning

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import '../style/Header.css';
+import '../style/PlanetCard.css';
 
 export default class PlanetCard extends Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <div className="planet-card__childDiv--align" data-testid="planet-card">
         <p className="page__text" data-testid="planet-name">{planetName}</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <img
+          className={ planetName }
+          src={ planetImage }
+          alt={ `Planeta ${planetName}` }
+        />
       </div>
     );
   }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import Title from './Title';
 import planets from '../data/planets';
 import PlanetCard from './PlanetCard';
+import '../style/SolarSystem.css';
 
 export default class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        {
-          planets.map((planet, index) => (
-            <PlanetCard
-              key={ index }
-              planetName={ planet.name }
-              planetImage={ planet.image }
-            />))
-        }
+        <div className="solar-system__div--align">
+          {
+            planets.map((planet, index) => (
+              <PlanetCard
+                key={ index }
+                planetName={ planet.name }
+                planetImage={ planet.image }
+              />))
+          }
+        </div>
       </div>
     );
   }
